6b8d783607935f30958fb09e934e1d8036eaee5c
Ivan Panchenko <arigatory@gmail.com>
2023-12-28 07:43:33 +0300

    add snippets

---
+++ src/app/snippets/new/page.tsx
@@ -0,0 +1,55 @@
+import { db } from "@/db";
+import { redirect } from "next/navigation";
+
+export default function SnippetCreatePage() {
+  async function createSnippet(formData: FormData) {
+    // this server action
+    "use server";
+
+    // check input
+    const title = formData.get("title") as string;
+    const code = formData.get("code") as string;
+
+    // create entity
+    const snippet = await db.snippet.create({
+      data: { title, code },
+    });
+    console.log(snippet);
+
+    // redirect
+    redirect("/");
+  }
+
+  return (
+    <form action={createSnippet}>
+      <h3 className="font-bold m-3">Create a Snippet</h3>
+      <div className="flex flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title" className=" w-12">
+            Title
+          </label>
+          <input
+            name="title"
+            className=" border rounded p-2 w-full"
+            type="text"
+          />
+        </div>
+
+        <div className="flex gap-4">
+          <label htmlFor="code" className=" w-12">
+            Code
+          </label>
+          <textarea
+            name="code"
+            id="code"
+            className=" border rounded p-2 w-full"
+          />
+        </div>
+
+        <button type="submit" className=" rounded p-2 bg-blue-200">
+          Create
+        </button>
+      </div>
+    </form>
+  );
+}
